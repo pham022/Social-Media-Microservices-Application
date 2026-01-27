9835d110c315b3bd6e71f2f24846fe9e6fda66ce
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-01-27 14:25:17 -0600

    create backend project

---
+++ project2-backend/src/test/java/com/example/project2_backend/Project2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
