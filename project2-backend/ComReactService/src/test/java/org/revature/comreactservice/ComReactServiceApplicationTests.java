edda6e6138de219b90f4fa97fff69f8cf1e97db5
jlcrogue <jlcrogue@gmail.com>
2026-01-30 14:45:45 -0700

    merged profile branch to fix pom issues

---
+++ project2-backend/ComReactService/src/test/java/org/revature/comreactservice/ComReactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.revature.comreactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComReactServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
