f16c6faaf062f2f9f8ee685dcf4d6e271e5c1df8
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-02-02 13:58:20 -0600

    add Auth Service (#9)
    * Add register and login endpoints
    
    * Add JWT authentication

---
+++ project2-backend/AuthService/src/main/java/com/example/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private Long userId;
+    private String token;
+    private String username;
+    private String email;
+    private String message;
+}
